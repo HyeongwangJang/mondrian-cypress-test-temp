bc443145070b93e41afdf86b1455bd177c7c6bdb
HyeongwangJang <hgjang@sphinfo.co.kr>
2024-02-27 16:47:39 +0900

    테스트 코드 완성

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,11 +1,23 @@
 describe('Test Mondrian', () => {
-  it('구글 맵 객체 확인', () => {
+  before(() => {
+    cy.intercept('POST', 'https://mondrian.sphinfo.co.kr:9443/$rpc/google.internal.maps.mapsjs.v1.MapsJsInternalService/GetViewportInfo').as('getViewportRequest');
+
     cy.visit('/');
 
     cy.contains('몬드리안 테스트').click();
 
     cy.url().should('include', '/js-api/mondrian.html');
 
+  })
+
+  it('GetViewportInfo must success', () => {
+    cy.wait('@getViewportRequest').its('response.statusCode').should('eq', 200); // now is intercepted
+
+    cy.wait(1000);
+  });
+
+  // 구글 맵 객체 확인
+  it('It must have google.maps.Map property', () => {
     // Maps JS API가 불러져왔는지 확인
     // google.maps.Map가 존재한다면 성공
     cy.wait(1500);
@@ -20,34 +32,47 @@ describe('Test Mondrian', () => {
     cy.wait(1000);
   });
 
-  it('위성 지도 타입 테스트', () => {
+  // // 위성 지도 타입 테스트
+  it('Map should be changed into satellite type', () => {
+    cy.intercept('GET', 'https://mondrian.sphinfo.co.kr:9443/google/jsapi/res/hskhms1_googleapis/kh**').as('stTileRequest');
+
     cy.get('#satellite-btn').click();
     cy.window().then((win) => {
       expect(win.map.getMapTypeId()).to.equal('satellite');
+      cy.wait('@stTileRequest').its('response.statusCode').should('eq', 200);
     });
     
     cy.wait(1000);
   });
 
-  it('하이브리드 지도 타입 테스트', () => {
+  // 하이브리드 지도 타입 테스트
+  it('Map should be changed into hybrid type', () => {
+    cy.intercept('GET', 'https://mondrian.sphinfo.co.kr:9443/google/jsapi/res/hsmaps_google/maps/vt**').as('hbTileRequest');
+
     cy.get('#hybrid-btn').click();
     cy.window().then((win) => {
       expect(win.map.getMapTypeId()).to.equal('hybrid');
+      cy.wait('@hbTileRequest').its('response.statusCode').should('eq', 200);
     });
 
     cy.wait(1000);
   });
 
-  it('지형정보 지도 타입 테스트', () => {
+  // 지형정보 지도 타입 테스트
+  it('Map should be changed into terrain type', () => {
+    cy.intercept('GET', 'https://mondrian.sphinfo.co.kr:9443/google/jsapi/res/hsmaps_google/maps/vt**').as('trTileRequest');
+
     cy.get('#terrain-btn').click();
     cy.window().then((win) => {
       expect(win.map.getMapTypeId()).to.equal('terrain');
+      cy.wait('@trTileRequest').its('response.statusCode').should('eq', 200);
     });
 
     cy.wait(1000);
   })
 
-  it('SNU 장학빌딩 마커 생성', () => {
+  // // SNU 장학빌딩 마커 생성
+  it('SNU building marker should be created', () => {
     cy.window().then((win) => {
       new win.google.maps.Marker({
         position: new win.google.maps.LatLng(37.542465, 126.95),
@@ -58,186 +83,43 @@ describe('Test Mondrian', () => {
     cy.wait(1000);
   });
 
-  it('쿼리에서 장소 찾기: 마포 우체국', () => {
-    cy.window().then((win) => {
-      const request = {
-        query: '마포 우체국',
-        fields: ["name", "geometry"],
-      };
-      new win.google.maps.places.PlacesService(win.map).findPlaceFromQuery(request, function (results, status) {
-        if (status === win.google.maps.places.PlacesServiceStatus.OK) {
-          for (var i = 0; i < results.length; i++) {
-            new win.google.maps.Marker({
-              position: results[i].geometry.location,
-              map: win.map,
-              title: results[i].name,
-            });
-          }
-          win.map.setCenter(results[0].geometry.location);
-        }
-      });
-    });
+  // 쿼리에서 장소 찾기: 마포 우체국
+  it('Place should be found from query: mapo postoffice', () => {
+    cy.intercept('GET', 'https://mondrian.sphinfo.co.kr:9443/maps/api/place/js/PlaceService.FindPlaceFromText**').as('findPlaceFromTextRequest');
+
+    cy.get('#place-btn').click();
+
+    cy.wait('@findPlaceFromTextRequest').its('response.statusCode').should('eq', 200);
 
     cy.wait(1000);
   });
 
-  it('캄팔라에서 나이로비 길 찾기', () => {
-    cy.window().then((win) => {
-      const fromLatLng = { lat: 0.33155226987432335, lng: 32.578180987282636 };
-      const toLatLng = { lat: -1.3070491855381845, lng: 36.81871450540919 };
+  // 캄팔라에서 나이로비 길 찾기
+  it('Direction should be found: from Kampala to Nairobi', () => {
+    cy.intercept('GET', 'https://mondrian.sphinfo.co.kr:9443/maps/api/js/DirectionsService.Route**').as('directionRequest');
 
-      new win.google.maps.DirectionsService().route(
-        {
-          origin: {
-            query: fromLatLng.lat + "," + fromLatLng.lng,
-          },
-          destination: {
-            query: toLatLng.lat + "," + toLatLng.lng,
-          },
-          travelMode: win.google.maps.TravelMode.DRIVING,
-        },
-        (res, status) => {
-          if (status === "OK") {
-            new win.google.maps.DirectionsRenderer().setDirections(res);
-          } else {
-            throw new Error();
-          }
-        }
-      );
-    });
+    cy.get('#direction-btn').click();
+
+    cy.wait('@directionRequest').its('response.statusCode').should('eq', 200);
 
     cy.wait(1000);
   });
+  
+  // 지오코딩 테스트
+  it('Geocoding test', () => {
+    cy.intercept('GET', 'https://mondrian.sphinfo.co.kr:9443/maps/api/js/GeocodeService.Search**').as('geocodingRequest');
 
-  it('지오코딩', () => {
-    cy.window().then((win) => {
-      new win.google.maps.Geocoder().geocode({ address: '광화문' }, (res, status) => {
-        if (status === "OK") {
-          const location = res[0].geometry.location;
-          new win.google.maps.Marker({
-            map: win.map,
-            position: location,
-          });
+    cy.get('#geocoding-btn').click();
 
-          win.map.setCenter(location);
-          win.map.setZoom(15);
-        } else {
-          throw new Error('지오코딩 실패');
-        }
-      });
-    });
+    cy.wait('@geocodingRequest').its('response.statusCode').should('eq', 200);
+
+    cy.wait(1000);
+  })
+
+  // 그리기 테스트
+  it('Triangle must be created', () => {
+    cy.get('#triangle-btn').click();
 
     cy.wait(1000);
   })
 })
-// describe('Test Mondrian', () => {
-//   it('test [몬드리안 테스트] page', () => {
-//     cy.visit('/');
-
-//     // 몬드리안 테스트 페이지 방문
-//     cy.contains('몬드리안 테스트').click();
-
-//     cy.url().should('include', '/js-api/mondrian.html');
-
-//     // Maps JS API가 불러져왔는지 확인
-//     // google.maps.Map가 존재한다면 성공
-//     cy.wait(2000);
-
-//     cy.window().should('have.property', 'google')
-//       .and('have.property', 'maps')
-//       .and('have.property', 'Map')
-//       .then((Map) => {
-//         cy.wrap(Map).as('Map');
-//       });
-
-//     // 위성 지도 타입 테스트
-//     cy.get('#satellite-btn').click();
-//     cy.window().then((win) => {
-//       expect(win.map.getMapTypeId()).to.equal('satellite');
-//     });
-
-//     // 하이브리드 지도 타입 테스트
-//     cy.get('#hybrid-btn').click();
-//     cy.window().then((win) => {
-//       expect(win.map.getMapTypeId()).to.equal('hybrid');
-//     });
-
-//     // 지형정보 지도 타입 테스트
-//     cy.get('#terrain-btn').click();
-//     cy.window().then((win) => {
-//       expect(win.map.getMapTypeId()).to.equal('terrain');
-//     });
-
-//     // SNU 장학빌딩 마커 생성
-//     cy.window().then((win) => {
-//       new win.google.maps.Marker({
-//         position: new win.google.maps.LatLng(37.542465, 126.95),
-//         map: win.map,
-//       });
-//     });
-
-//     // 쿼리에서 장소 찾기: 마포 우체국
-//     cy.window().then((win) => {
-      
-//       const request = {
-//         query: '마포 우체국',
-//         fields: ["name", "geometry"],
-//       };
-//       new win.google.maps.places.PlacesService(win.map).findPlaceFromQuery(request, function (results, status) {
-//         if (status === win.google.maps.places.PlacesServiceStatus.OK) {
-//           for (var i = 0; i < results.length; i++) {
-//             new win.google.maps.Marker({
-//               position: results[i].geometry.location,
-//               map: win.map,
-//               title: results[i].name,
-//             });
-//           }
-//           win.map.setCenter(results[0].geometry.location);
-//         }
-//       });
-//     });
-
-//     // 캄팔라에서 나이로비 길 찾기
-//     cy.window().then((win) => {
-//       const fromLatLng = { lat: 0.33155226987432335, lng: 32.578180987282636 };
-//       const toLatLng = { lat: -1.3070491855381845, lng: 36.81871450540919 };
-
-//       new win.google.maps.DirectionsService().route(
-//         {
-//           origin: {
-//             query: fromLatLng.lat + "," + fromLatLng.lng,
-//           },
-//           destination: {
-//             query: toLatLng.lat + "," + toLatLng.lng,
-//           },
-//           travelMode: win.google.maps.TravelMode.DRIVING,
-//         },
-//         (res, status) => {
-//           if (status === "OK") {
-//             new win.google.maps.DirectionsRenderer().setDirections(res);
-//           } else {
-//             throw new Error();
-//           }
-//         }
-//       );
-//     });
-
-//     // 지오코딩
-//     cy.window().then((win) => {
-//       new win.google.maps.Geocoder().geocode({ address: '광화문' }, (res, status) => {
-//         if (status === "OK") {
-//           const location = res[0].geometry.location;
-//           new win.google.maps.Marker({
-//             map: win.map,
-//             position: location,
-//           });
-
-//           win.map.setCenter(location);
-//           win.map.setZoom(15);
-//         } else {
-//           throw new Error('지오코딩 실패');
-//         }
-//       });
-//     });
-//   });
-// })
